3bca528c45fc90e15bf585697527eeec7f23db64
sibghataaha <38325762+sibghataaha@users.noreply.github.com>
2018-04-16 16:14:08 +0500

    Dealdl class
    Saving all the deals in a list.
    All the functions related to deals are in this class.

---
+++ Dealdl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1
+{
+    public class Dealdl
+    {
+        public static List<Deal> deals = new List<Deal>();
+        public void AddDeal(Deal dl)
+         {
+            //Add code here
+         }
+        public void deleteDeal1(Deal d2) //by admin
+        {
+            //Add code here
+        }
+        public void OrderDeal(Deal d3, int quantity)
+        {
+            //Add code here
+        }
+        public void deleteDeal2(Deal d4)
+        {
+          //Add code here
+        }
+
+
+    }
+   
+    
+}
