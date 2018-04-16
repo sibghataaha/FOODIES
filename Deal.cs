9e444c066c1b437aa1ba5de0d6609589b76ec9d7
sibghataaha <38325762+sibghataaha@users.noreply.github.com>
2018-04-16 16:01:59 +0500

    Class Deal
    All the required attributes of deals are in this class

---
+++ Deal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1
+{
+    public class Deal
+    {
+        private Item itemselected;
+
+        public Item Itemselected
+        {
+            get { return itemselected; }
+            set { itemselected = value; }
+        }
+        private int price;
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private int subtotal;
+
+        public int Subtotal
+        {
+            get { return subtotal; }
+            set { subtotal = value; }
+        }
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set { total = value; }
+        }
+        private int tax;
+
+        public int Tax
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+
+    }
+}
