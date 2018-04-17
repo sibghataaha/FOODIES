f279137beefd74c7cd59d19e54724d3de1d35939
sibghataaha <38325762+sibghataaha@users.noreply.github.com>
2018-04-17 06:27:08 +0500

    Location class
    Have the adresses of branches of foodies in the form of longitude and latitude

---
+++ Loaction.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1
+{
+    public class Loaction
+    {
+                private const double latitude1 = 31.538;
+
+public double Latitude1
+{
+  get { return latitude1; }  
+} 
+
+        private const double longitude1 = 74.3358;
+
+public double Longitude1 
+{
+  get { return longitude1; }  
+} 
+        private const double latitude2 = 32.5866;
+
+public double Latitude2
+{
+  get { return latitude2; }  
+} 
+
+        private const double longitude2 = 74.3329 ;
+
+public double Longitude2
+{
+  get { return longitude2; }  
+}
+
+private const double latitude3 = 31.5323;
+
+public double Latitude3
+{
+    get { return latitude3; }
+}
+
+private const double longitude3 = 74.3630;
+
+public double Longitude3
+{
+    get { return longitude3; }
+}
+
+private string useradress;
+
+public string Useradress
+{
+    get { return useradress; }
+    set { useradress = value; }
+}
+    }
+}
