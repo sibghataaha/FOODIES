1660fec67f16e4003bbbd06e35e3419ea3b07f38
sibghataaha <38325762+sibghataaha@users.noreply.github.com>
2018-04-17 06:28:09 +0500

    location dl

---
+++ Locationdl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project1
+{
+    public class Locationdl
+    {
+        public List<Loaction> l = new List<Loaction>();
+
+        public Loaction nearbylocation(string useraddress)
+    {
+        //add code here
+    }
+    }
+}
